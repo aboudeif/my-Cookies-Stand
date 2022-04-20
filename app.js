6f55dcb0a54949771dc269d6014e8b255c6cd000
Abdallah Aboudeif <comofcom@gmail.com>
2022-04-20 19:39:28 +0200

    store and retrieve data

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const table= document.getElementById('table');
 const addForm=document.getElementById('myForm');
 let hoursOfWork=['6am ','7am ','8am ','9am ','10am ','11am ','12am ','1pm ','2pm ','3pm ','4pm ','5pm ','6pm','7pm'];
-let stores=[];
-if(JSON.parse(localStorage.getItem('sales'))) stores = JSON.parse(localStorage.getItem('sales'))
+let stores= localStorage.getItem('sales') ? JSON.parse(localStorage.getItem('sales')):[];
 buildTableHeader();
-if(stores) fillStores();
+if(stores){
+    fillStores();
+    //console.log(JSON.parse(localStorage.getItem('sales')))
+}
 
 // tableheader
 
@@ -22,8 +24,6 @@ function buildTableHeader(){
     thead.appendChild(row)
     table.appendChild(thead)
     }
-    
-    
 
     function storeSales(name,maxcus,mincus,avgsales){
         this.name = name
@@ -43,15 +43,15 @@ function buildTableHeader(){
             this.cookiesPerHour.push(Math.floor(this.avgsales * this.getRandomCustomers()))
     }
 
-    storeSales.prototype.render = function(){
+    function render(store){
     let tbody= document.createElement('tbody')
     let row = document.createElement('tr')
     let col = document.createElement('td')
-    col.textContent = this.name
+    col.textContent = store.name
     row.appendChild(col)
-    for(let i = 0 ; i<this.cookiesPerHour.length;i++){
+    for(let i = 0 ; i<store.cookiesPerHour.length;i++){
         col = document.createElement('td')
-        col.textContent = this.cookiesPerHour[i]
+        col.textContent = store.cookiesPerHour[i]
     row.appendChild(col)
     }
     tbody.appendChild(row)
@@ -63,12 +63,15 @@ function buildTableHeader(){
     // const dubai = new storeSales('Dubi',38,11,3.7);
     // const paris = new storeSales('Paris',38,20,2.3);
     // const lima = new storeSales('lima',16,2,4.6);
+    
     function fillStores(){
         stores.forEach(store => {
-            store.getHourlySales()
-            store.render()
-            
-        })}
+            // store = new storeSales(store.name, store.maxcus, store.mincus, store.avgsales)
+            // store.getHourlySales()
+            // console.log(store)
+            stores.push(store)
+            render(store)
+        });}
 
     addForm.addEventListener('submit', e =>{
         e.preventDefault()
@@ -78,8 +81,8 @@ function buildTableHeader(){
         const avg = e.target.avgCookies.value
         const newStore = new  storeSales(name,max,min,avg)
         newStore.getHourlySales()
-        newStore.render()
-        stores.push(newStore)
-        localStorage.setItem('store',JSON.stringify(stores))
+        render(newStore)
+        // stores.push(newStore)
+        localStorage.setItem('sales',JSON.stringify(stores))
         addForm.reset()
     })
